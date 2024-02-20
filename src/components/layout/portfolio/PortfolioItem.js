38328249c0f96cf2a7b1bc300583ba53220b5d69
Masouma <masouma2020@gmail.com>
2024-02-20 11:44:32 +0430

    Edit image size for portfolio

--- src/components/layout/portfolio/PortfolioItem.js
+++ src/components/layout/portfolio/PortfolioItem.js
@@ -1,29 +1,50 @@
 import React from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import { Col, Card, Image } from "react-bootstrap";
 
-const PortfolioItem = ({ item , onItemClick }) => {
-    const handleClick = () => {
-        onItemClick(item);
-      };
+const PortfolioItem = ({ item, onItemClick }) => {
+  const handleClick = () => {
+    onItemClick(item);
+  };
+
+  const imageStyle = {
+    height: "250px", // Adjust the height as needed
+    weight: "250px", // Adjust the height as needed
+    objectFit: "cover",
+  };
+
   return (
-    <div className="col-lg-4 col-md-6 portfolio-item filter-app" >
-      <div className="portfolio-wrap">
-      <img src={item.imageUrlMain} className="img-fluid fixed-height" alt={item.title} />
-        <div className="portfolio-info">
-          <h4>{item.title}</h4>
-          <p>{item.category}</p>
+    <Col lg={4} md={6} className="portfolio-item filter-app">
+      <Card className="portfolio-wrap">
+        <div className="img-container">
+          <Image src={item.imageUrlMain} style={imageStyle} alt={item.title} />
+        </div>
+        <Card.Body className="portfolio-info">
+          <Card.Title>{item.title}</Card.Title>
+          <Card.Text>{item.category}</Card.Text>
           <div className="portfolio-links">
-            <a href={item.imageUrl} data-gallery="portfolioGallery" className="portfolio-lightbox" title={item.title}>
+            <a
+              href={item.imageUrl}
+              data-gallery="portfolioGallery"
+              className="portfolio-lightbox"
+              title={item.title}
+            >
               <i className="bx bx-plus"></i>
             </a>
-           
-               <Link to="#" data-gallery="portfolioDetailsGallery" data-glightbox="type: external" className="portfolio-details-lightbox" title="Portfolio Details" onClick={handleClick}>
-                <i className="bx bx-link"></i>
-              </Link>
+            <Link
+              to="#"
+              data-gallery="portfolioDetailsGallery"
+              data-glightbox="type: external"
+              className="portfolio-details-lightbox"
+              title="Portfolio Details"
+              onClick={handleClick}
+            >
+              <i className="bx bx-link"></i>
+            </Link>
           </div>
-        </div>
-      </div>
-    </div>
+        </Card.Body>
+      </Card>
+    </Col>
   );
 };
 
